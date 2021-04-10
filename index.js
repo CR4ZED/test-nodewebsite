da65ff8be517726728fd9a86edea3fd13ce46933
CR4ZED <67179973+CR4ZED@users.noreply.github.com>
2021-04-10 10:21:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("hello");
+  res.end("<h1>Hello this is a test node website</h1>");
 });
 
 server.listen(PORT, () => console.log(`server is listening at PORT ${PORT}`));
